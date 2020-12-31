91c3e5391ca76a5ca447b89e1d2c5e5a1ad3ad51
koolii <koolii0909@gmail.com>
2020-12-31 11:50:22 +0900

    Update Greeting page (useRouter)

--- pages/hello/[name].tsx
+++ pages/hello/[name].tsx
@@ -1,10 +1,18 @@
-import { useRouter } from 'next/dist/client/router';
 import React from 'react';
+// import { useRouter } from 'next/router';
 
-const Greeting: React.FC = () => {
-  const router = useRouter();
-  const { name } = router.query;
-  return <div>Hello {name}</div>;
-};
+// const Greeting: React.FC = () => {
+//   const router = useRouter();
+//   const { name } = router.query;
+//   return <div>Hello {name}</div>;
+// };
+
+// useRouterだとクライアント側で処理するために、レンダリング時のチラつきが気になるかもしれません。
+// その場合はgetServerSidePropsを利用しましょう
+interface Props {
+  name: string;
+}
+const Greeting: React.FC<Props> = ({ name }) => <div>Hello {name}</div>;
 
 export default Greeting;
+export const getServerSideProps = (): { props: Props } => ({ props: { name: 'koolii' } });
