564fbbe7a44ac6d3844a18f8e42ca6684167d432
koolii <koolii0909@gmail.com>
2020-12-31 14:32:18 +0900

    Add api middlewarer

--- pages/api/holidays/[year].ts
+++ pages/api/holidays/[year].ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'node-fetch';
+import middleware from '../../../src/middleware';
 
 interface HolidayOrigin {
   date: string;
@@ -16,21 +17,23 @@ export interface Holiday extends HolidayOrigin {
 }
 
 // CORS制限を外す為にProxyする
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  res.setHeader('Content-Type', 'application/json');
+export default middleware(
+  async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+    res.setHeader('Content-Type', 'application/json');
 
-  // 外部のリクエストを取得
-  const { year } = req.query;
-  const result = await fetch(`https://date.nager.at/api/v2/PublicHolidays/${year}/JP`);
-  if (result.ok) {
-    // json化
-    const holidays = (await result.json()) as HolidayOrigin[];
-    // FEにjson形式でレスポンスを返却
-    const response: Holiday[] = holidays.map((h, idx) => ({ id: idx, ...h }));
-    res.statusCode = 200;
-    res.json(response);
-  }
+    // 外部のリクエストを取得
+    const { year } = req.query;
+    const result = await fetch(`https://date.nager.at/api/v2/PublicHolidays/${year}/JP`);
+    if (result.ok) {
+      // json化
+      const holidays = (await result.json()) as HolidayOrigin[];
+      // FEにjson形式でレスポンスを返却
+      const response: Holiday[] = holidays.map((h, idx) => ({ id: idx, ...h }));
+      res.statusCode = 200;
+      res.json(response);
+    }
 
-  res.statusCode = 500;
-  res.end();
-};
+    res.statusCode = 500;
+    res.end();
+  },
+);
